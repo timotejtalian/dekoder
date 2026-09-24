6502cfffa1abaaaeed0373e6094f8df486112679
timotejtalian <timotej.talian15@gmail.com>
2026-09-24 22:01:32 +0200

    14

--- maturitne zadfania/14.py
+++ maturitne zadfania/14.py
@@ -1,17 +1,37 @@
 f = open("skosky.txt", "r")
 
+sportovci = {}
+krajiny = {}
 
-kraj = []
 for riadok in f:
     riadok = riadok.strip()
     cast = riadok.split()
+    
+    meno = cast[0]
     krajina = cast[1]
-    prvy_vykon = cast[2]
-    druhy_v = cast[3]
-    treti_v = cast[4]
-    stvrty_v = cast[5]
-    piaty_v = cast[6]
-    for i in range(0, sum(1 for line in f) + 1):
-        kraj.append(krajina)
-    k = list(set(kraj))
-print(k)
+    skoky = list(map(int, cast[2:]))
+
+    sportovci[meno] = max(skoky)
+
+    if krajina in krajiny:
+        krajiny[krajina] += 1
+    else:
+        krajiny[krajina] = 1
+
+print("Krajiny a počet športovcov:")
+for krajina in krajiny:
+    print(krajina, krajiny[krajina])
+
+naj_skok = 0
+vitaz = []
+
+for meno in sportovci:
+    if sportovci[meno] > naj_skok:
+        naj_skok = sportovci[meno]
+        vitez = [meno]
+    elif sportovci[meno] == naj_skok:
+        vitez.append(meno)
+
+print("Najvyšší skok má:")
+for meno in vitez:
+    print(meno)
